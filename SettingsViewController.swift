cf7b5a0f492acf39bd586a7b3db9f333bb201630
jsingh <waheguru~8>
2016-09-26 14:57:07 -0700

    Adding the gritti app, used for calculating tip

---
+++ SettingsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsViewController.swift
+//  gratti
+//
+//  Created by Singh, Jagdeep on 9/26/16.
+//  Copyright © 2016 Singh, Jagdeep. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTipSegment: UISegmentedControl!
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if(defaults.objectForKey("defaultTip") != nil){
+            defaultTipSegment.selectedSegmentIndex = defaults.integerForKey("defaultTip")
+        }
+
+    }
+    @IBAction func updateDefaultTip(sender: AnyObject) {
+        
+        defaults.setInteger(defaultTipSegment.selectedSegmentIndex, forKey: "defaultTip")
+        
+    }
+
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ gratti/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  gratti
+//
+//  Created by Singh, Jagdeep on 9/25/16.
+//  Copyright © 2016 Singh, Jagdeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults();
+        if(defaults.objectForKey("defaultTip") != nil){
+            tipControl.selectedSegmentIndex = defaults.integerForKey("defaultTip");
+            calculateTip(tipControl);
+        }
+        
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true);
+        
+    }
+ 
+    
+
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercent = [0.15, 0.18, 0.20];
+        
+        let bill = Double(billField.text!) ?? 0;
+        let tip = bill * tipPercent[tipControl.selectedSegmentIndex];
+        let total = bill + tip;
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2 f", total)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
